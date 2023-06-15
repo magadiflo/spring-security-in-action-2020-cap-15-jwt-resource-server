c49e3aa25ca6a60dc362c1ea0f10bf8302cbb0d1
Martín <magadiflo@gmail.com>
2023-06-15 11:07:01 -0500

    Consumiendo clave pública expuesta por el servidor de autorización

--- src/main/java/com/magadiflo/book/security/app/config/ResourceServerConfig.java
+++ src/main/java/com/magadiflo/book/security/app/config/ResourceServerConfig.java
@@ -1,35 +1,11 @@
 package com.magadiflo.book.security.app.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @EnableResourceServer
 @Configuration
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
-    @Value("${publicKey}")
-    private String publicKey;
 
-    @Override
-    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        resources.tokenStore(this.tokenStore());
-    }
-
-    @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(this.jwtAccessTokenConverter());
-    }
-
-    @Bean
-    public JwtAccessTokenConverter jwtAccessTokenConverter() {
-        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setVerifierKey(this.publicKey);
-        return converter;
-    }
 }
